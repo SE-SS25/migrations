6c95fa33ee93088014ebed1dc1515fa85415163f
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-06-23 15:01:09 +0200

    Update metrics

--- 20250620095806_worker_metric.up.sql
+++ 20250620095806_worker_metric.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE worker_metric
 (
-    id              UUID PRIMARY KEY,
-    last_heartbeat  TIMESTAMPTZ NOT NULL,
-    uptime          INTERVAL    NOT NULL DEFAULT '0ms',
-    req_per_sec     INT         NOT NULL DEFAULT 0,
-    write_per_sec   INT         NOT NULL DEFAULT 0,
-    read_per_sec    INT         NOT NULL DEFAULT 0,
-    req_total       BIGINT      NOT NULL DEFAULT 0,
-    req_failed      BIGINT      NOT NULL DEFAULT 0,
-    db_availability REAL        NOT NULL DEFAULT 0.0
+    id             UUID PRIMARY KEY,
+    last_heartbeat TIMESTAMPTZ NOT NULL,
+    uptime         INTERVAL    NOT NULL DEFAULT '0ms',
+    req_per_sec    REAL        NOT NULL DEFAULT 0.0,
+    read_per_sec   REAL        NOT NULL DEFAULT 0.0,
+    write_per_sec  REAL        NOT NULL DEFAULT 0.0,
+    req_total      BIGINT      NOT NULL DEFAULT 0,
+    req_failed     BIGINT      NOT NULL DEFAULT 0,
+    db_err_rate    REAL        NOT NULL DEFAULT 0.0
 );
