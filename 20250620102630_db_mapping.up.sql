c6c61fa23aaa29d5868ee6874d12235484830851
linusgustafsson <linusgustafsson202@gmail.com>
2025-06-25 13:05:37 +0200

    Fixed typo

--- 20250620102630_db_mapping.up.sql
+++ 20250620102630_db_mapping.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE db_mapping
 (
     id          UUID PRIMARY KEY,
-    m_worker_id UUID NOT NULL UNQIUE referencesmigration_worker(id),
+    m_worker_id UUID NOT NULL UNIQUE references migration_worker(id),
     url         TEXT NOT NULL,
     "from"      TEXT NOT NULL UNIQUE,
     "to"        TEXT NOT NULL UNIQUE
