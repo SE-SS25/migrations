73477da05890796f75e0618903c29b01fea8d0b7
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-06-27 12:02:41 +0200

    Remove "to" fro, db mappings

--- 20250620102630_db_mapping.up.sql
+++ 20250620102630_db_mapping.up.sql
@@ -3,6 +3,5 @@ CREATE TABLE db_mapping
     id          UUID PRIMARY KEY,
     m_worker_id UUID NOT NULL UNIQUE references migration_worker (id),
     url         TEXT NOT NULL,
-    "from"      TEXT NOT NULL UNIQUE,
-    "to"        TEXT NOT NULL UNIQUE
+    "from"      TEXT NOT NULL UNIQUE
 );
