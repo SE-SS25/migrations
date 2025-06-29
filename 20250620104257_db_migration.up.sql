ec87b35893fa1f3c9d1dff0e0a3f6a5f11716068
linusgustafsson <linusgustafsson202@gmail.com>
2025-06-29 12:03:23 +0200

    added status attribute to db_migration table

--- 20250620104257_db_migration.up.sql
+++ 20250620104257_db_migration.up.sql
@@ -4,5 +4,6 @@ CREATE TABLE db_migration
     url         TEXT NOT NULL UNIQUE,
     m_worker_id UUID NOT NULL UNIQUE references migration_worker (id),
     "from"      TEXT NOT NULL UNIQUE,
-    "to"        TEXT NOT NULL UNIQUE
+    "to"        TEXT NOT NULL UNIQUE,
+    status      TEXT NOT NULL,
 );
