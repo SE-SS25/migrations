a436718cd75b26fc81a673bf0245a51fa3a85161
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-06-29 17:31:14 +0200

    Timezones are important

--- 20250620102811_db_conn_err.up.sql
+++ 20250620102811_db_conn_err.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE db_conn_err
 (
-    err_id UUID PRIMARY KEY,
+    err_id    UUID PRIMARY KEY,
     worker_id UUID REFERENCES worker_metric (id),
     db_url    TEXT REFERENCES db_mapping (url),
-    fail_time TIMESTAMP NOT NULL
+    fail_time TIMESTAMPTZ NOT NULL
 );
