1bc0aaee41d77217b29948bdac604d37324cb3ec
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-06-29 19:59:08 +0200

    Add url reference

--- 20250620101426_monitor.up.sql
+++ 20250620101426_monitor.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE monitor
 (
     id             UUID PRIMARY KEY,
     last_heartbeat TIMESTAMPTZ NOT NULL,
-    url            TEXT        NOT NULL
+    url            TEXT        NOT NULL REFERENCES db_instance (url)
 );
