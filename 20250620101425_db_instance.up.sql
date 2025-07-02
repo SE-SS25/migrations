b7f5cbbc95086dc906135ddacdcc99f3f1c55202
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-07-02 12:43:32 +0200

    Reformat

--- 20250620101425_db_instance.up.sql
+++ 20250620101425_db_instance.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE db_instance
     url              TEXT PRIMARY KEY,
     created          TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     max_space        BIGINT      NOT NULL, --size in bytes; maybe default?
-    occupied_space   BIGINT,              --in bytes
+    occupied_space   BIGINT,               --in bytes
     collection_count BIGINT,
     last_queried     TIMESTAMPTZ
 );
