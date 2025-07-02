3f3aec5261a604908fc50973700fa41a9e934fd4
linusgustafsson <linusgustafsson202@gmail.com>
2025-07-02 11:11:10 +0200

    changed db tables to support storing information that are relevant to the user to make scaling / migration decisions, namely disk space of the dbs

--- 20250620101425_db_instance.up.sql
+++ 20250620101425_db_instance.up.sql
@@ -1,5 +1,9 @@
 CREATE TABLE db_instance
 (
-    url     TEXT PRIMARY KEY,
-    created TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+    url              TEXT PRIMARY KEY,
+    created          TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    max_space        BIGINT      NOT NULL, --size in bytes; maybe default?
+    occupied_space   BIGINT,              --in bytes
+    collection_count BIGINT,
+    last_queried     TIMESTAMPTZ
 );
--- 20250620102630_db_mapping.up.sql
+++ 20250620102630_db_mapping.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE db_mapping
 (
     id     UUID PRIMARY KEY,
-    url    TEXT NOT NULL REFERENCES db_instance (url),
-    "from" TEXT NOT NULL UNIQUE
+    url    TEXT   NOT NULL REFERENCES db_instance (url),
+    "from" TEXT   NOT NULL UNIQUE,
+    size   BIGINT NOT NULL
 );
