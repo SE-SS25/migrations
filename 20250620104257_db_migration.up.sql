9a57eafc73bee60ff5787832c652f8994b92592a
linusgustafsson <linusgustafsson202@gmail.com>
2025-07-02 12:43:52 +0200

    set to in migrations table so it may be null, since the controller creates the entry but the migration worker calculates "to"

--- 20250620104257_db_migration.up.sql
+++ 20250620104257_db_migration.up.sql
@@ -4,6 +4,6 @@ CREATE TABLE db_migration
     url         TEXT             NOT NULL UNIQUE,
     m_worker_id UUID             NOT NULL UNIQUE references migration_worker (id),
     "from"      TEXT             NOT NULL UNIQUE,
-    "to"        TEXT             NOT NULL UNIQUE,
+    "to"        TEXT             UNIQUE,
     status      MIGRATION_STATUS NOT NULL
 );
