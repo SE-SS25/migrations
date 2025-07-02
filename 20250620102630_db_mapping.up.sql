c329eb110bc1a7842900e7f8929fbb2d97d9613c
HaDeSMonsta <leon.poellinger+git@gmail.com>
2025-07-02 16:38:27 +0200

    Add default mapping uuid

--- 20250620102630_db_mapping.up.sql
+++ 20250620102630_db_mapping.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE db_mapping
 (
-    id     UUID PRIMARY KEY,
+    id     UUID PRIMARY KEY default uuid_generate_v4(),
     url    TEXT NOT NULL REFERENCES db_instance (url),
     "from" TEXT NOT NULL UNIQUE,
     size   BIGINT
